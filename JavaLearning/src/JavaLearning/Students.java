7986c24804823c719b2867da0b875645186254ae
Sushanti <Sushanti@ip-192-168-29-185.ap-south-1.compute.internal>
2024-06-13 21:28:51 +0530

    pushing the code first time.

---
+++ JavaLearning/src/JavaLearning/Students.java
@@ -0,0 +1,12 @@
+package JavaLearning;
+
+public class Students {
+
+	int age;
+	int rollNo;
+	public void sushanti()
+	{
+		System.out.println(" welcome to all of you");
+		System.out.println("Automation is very easy");
+	}
+}
